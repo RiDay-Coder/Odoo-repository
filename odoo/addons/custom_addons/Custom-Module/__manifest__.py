781eac018aecf5fff42f1b01eddfb8a8ac62d865
hirdyanshugoyal <goyalh@uci.edu>
2025-04-15 01:42:56 +0530

    Odoo basic Setup and custom module template

---
+++ odoo/addons/custom_addons/Custom-Module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'My Custom Module',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': 'A simple custom module for learning purposes.',
+    'author': 'Your Name',
+    'website': 'http://yourcompany.com',
+    'depends': ['base'],  # Base is the minimal dependency
+    'data': [
+        'views/my_model_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
+
